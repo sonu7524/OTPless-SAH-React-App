c507364255c270a78f363c2f2ef45a28446a81b4
sonu7524 <sonudaryani135@gmail.com>
2023-12-01 13:17:57 +0530

    update

--- src/components/Auth/Login/index.jsx
+++ src/components/Auth/Login/index.jsx
@@ -1,16 +1,7 @@
 import React, { useState } from "react";
-import loginImg from "../../../assets/login.png";
 import "./styles.css";
-import axios from "axios";
-import { Link } from "react-router-dom";
-import { Button } from "@mui/material";
-import ButtonComponent from "../../common/Button";
-import ScriptTag from 'react-script-tag';
 
 const Login = () => {
-    let[email,setEmail] = useState("");
-    let[password,setPassword] = useState("");
-    let[error,setError] = useState("");
 
     useEffect(() => {
       window.otpless = (otplessUser) => {
@@ -22,7 +13,7 @@ const Login = () => {
                        
     return (
         <div className="login">
-          <div id="otpless-login-page"></div>
+          
         </div>
     )
 }
