37b30d39c21b547a543657549352eaab5c1f9ce4
sonu7524 <sonudaryani135@gmail.com>
2023-12-01 14:42:59 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,10 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Loader from "./components/common/Loader";
 import AccountPage from "./pages/AccountPage";
 const LoginPage = lazy(() => import("./pages/LoginPage"));
-const DashboardPage = lazy(() => import("./pages/DashboardPage"));
 import { ProductProvider } from "./ProductContext";
+const DashboardPage = lazy(() => import("./pages/DashboardPage"));
 
 function App() {
-  const authToken = sessionStorage.getItem("auth_token");
   return (
     <div className="App">
       <ProductProvider>
@@ -18,9 +17,10 @@ function App() {
         <Routes>
           <Route path="/register" element={<SignupPage />} />
           <Route path="/" element={<HomePage />} />
-          {authToken ? <Route path="/login" element={<Navigate to="/dashboard" />} /> : <Route path="/login" element={<LoginPage />} />}
-          {authToken ? <Route path="/dashboard" element={<DashboardPage />} /> : <Route path="/dashboard" element={<Navigate to="/login" />} />}
-          {authToken ? <Route path="/account" element={<AccountPage />} /> : <Route path="/account" element={<Navigate to="/login" />} />}
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="/account" element={<AccountPage />} />
+          <Route path="*" element={<Navigate to="/" />} />
           
         </Routes>
         </Suspense>
